ae32f079304aafcfab8451d1e39311edd765a26d
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-05-02 00:23:58 +0300

    add function to get all characters

--- app/src/main/java/com/example/marvel_hub/data/MarvelApiService.kt
+++ app/src/main/java/com/example/marvel_hub/data/MarvelApiService.kt
@@ -1,4 +1,14 @@
 package com.example.marvel_hub.data
 
+import com.example.marvel_hub.data.model.MarvelCharactersModel
+import com.example.marvel_hub.util.Constants
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MarvelApiService {
+    @GET("characters")
+    fun getAllCharacters(
+        @Query("apikey") apikey: String = Constants.ApiKey.getApiKeyWithHash()
+    ): Single<MarvelCharactersModel>
 }
