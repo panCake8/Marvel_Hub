ac195f7d3747922d7269115c1c15945ce857659b
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-05-02 00:23:39 +0300

    add hash to api key

--- app/src/main/java/com/example/marvel_hub/util/Constants.kt
+++ app/src/main/java/com/example/marvel_hub/util/Constants.kt
@@ -7,20 +7,25 @@ import java.sql.Timestamp
 
 object Constants {
 
-    private val timeStamp = Timestamp(System.currentTimeMillis()).time.toString()
-    private const val HASH_ALGORITHM = "MD5"
-    private const val RADIX = 16
-    private const val SIG_NUM = 1
-    private const val LENGTH = 1
-    private const val PAD_CHAR = '0'
-    fun toHash(): String {
-        val input = "$timeStamp${BuildConfig.PRIVATE_API_KEY}${BuildConfig.PUBLIC_API_KEY}"
-        val messageDigest = MessageDigest.getInstance(HASH_ALGORITHM)
-        return BigInteger(
-            SIG_NUM, messageDigest
-                .digest(input.toByteArray())
-        )
-            .toString(RADIX)
-            .padStart(LENGTH, PAD_CHAR)
+    object ApiKey {
+        private val timeStamp = Timestamp(System.currentTimeMillis()).time.toString()
+        private const val HASH_ALGORITHM = "MD5"
+        private const val RADIX = 16
+        private const val SIG_NUM = 1
+        private const val LENGTH = 1
+        private const val PAD_CHAR = '0'
+
+        fun getApiKeyWithHash(): String {
+            val input = "$timeStamp${BuildConfig.PRIVATE_API_KEY}${BuildConfig.PUBLIC_API_KEY}"
+            val messageDigest = MessageDigest.getInstance(HASH_ALGORITHM)
+            return BigInteger(
+                SIG_NUM, messageDigest
+                    .digest(input.toByteArray())
+            )
+                .toString(RADIX)
+                .padStart(LENGTH, PAD_CHAR)
+        }
     }
+
+
 }
