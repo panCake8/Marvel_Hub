d8b637dfed0dc726d998c20b1908bccf621c634d
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-05-02 22:26:41 +0300

    update activity

---
+++ app/src/main/java/com/example/marvel_hub/ui/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.marvel_hub.ui
+
+import com.example.marvel_hub.R
+import com.example.marvel_hub.databinding.ActivityHomeBinding
+import com.example.marvel_hub.ui.base.BaseActivity
+
+class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+    override val layoutId: Int
+        get() = R.layout.activity_home
+
+}
