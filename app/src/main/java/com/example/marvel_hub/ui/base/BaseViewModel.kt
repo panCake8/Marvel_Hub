30aa9e81bac4bfaf0e8e31bc147b6d68fb445b0f
mustafayassi <must3f3yassin@gmail.com>
2023-05-02 21:40:32 +0300

    create base view model and base fragment

---
+++ app/src/main/java/com/example/marvel_hub/ui/base/BaseViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.marvel_hub.ui.base
+
+import androidx.lifecycle.ViewModel
+import com.example.marvel_hub.data.repository.IMarvelRepository
+import com.example.marvel_hub.data.repository.MarvelRepository
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+
+abstract class BaseViewModel : ViewModel() {
+
+    protected val repository: IMarvelRepository = MarvelRepository()
+
+    private val disposables = CompositeDisposable()
+
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.dispose()
+    }
+}
