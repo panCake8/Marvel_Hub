62f8e6ecb6830f58b2a679c070b39c029c83fa9e
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-05-02 14:24:08 +0300

    add events requests

--- app/src/main/java/com/example/marvel_hub/data/api/MarvelApiService.kt
+++ app/src/main/java/com/example/marvel_hub/data/api/MarvelApiService.kt
@@ -1,6 +1,7 @@
 package com.example.marvel_hub.data.api
 
 import com.example.marvel_hub.data.model.CharactersModel
+import com.example.marvel_hub.data.model.EventModel
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,4 +15,39 @@ interface MarvelApiService {
     fun getCharacterById(
         @Path("characterId") characterId: Int,
     ): Single<CharactersModel>
+
+// events requests
+    @GET("v1/public/events")
+    fun getAllEvents(): Single<EventModel>
+
+    @GET("v1/public/events/{eventId}")
+    fun getEventsById(
+        @Path("eventId") eventId: Int
+    ): Single<EventModel>
+
+    @GET("v1/public/events/{eventId}/characters")
+    fun getCharactersByEventId(
+        @Path("eventId") eventId: Int
+    ): Single<EventModel>
+
+    @GET("v1/public/events/{eventId}/comics")
+    fun getComicsByEventId(
+        @Path("eventId") eventId: Int
+    ): Single<EventModel>
+
+    @GET("v1/public/events/{eventId}/creators")
+    fun getCreatorsByEventId(
+        @Path("eventId") eventId: Int
+    ): Single<EventModel>
+
+    @GET("v1/public/events/{eventId}/series")
+    fun getSeriesByEventId(
+        @Path("eventId") eventId: Int
+    ): Single<EventModel>
+
+    @GET("v1/public/events/{eventId}/series")
+    fun getStoriesByEventId(
+        @Path("eventId") eventId: Int
+    ): Single<EventModel>
+
 }
