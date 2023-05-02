2e51fd79ee92f117474506efc59a8aae2dadddda
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-05-02 22:25:16 +0300

    remove view model

--- app/src/main/java/com/example/marvel_hub/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/marvel_hub/ui/base/BaseActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.ViewModel
 
-abstract class BaseActivity<VD : ViewDataBinding, VM : BaseViewModel> : AppCompatActivity() {
+abstract class BaseActivity<VD : ViewDataBinding> : AppCompatActivity() {
 
     protected lateinit var binding: VD
-
-    abstract val viewModel: VM
 
     abstract val layoutId: Int
 
