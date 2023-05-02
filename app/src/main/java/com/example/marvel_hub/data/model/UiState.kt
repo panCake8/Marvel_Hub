42959a5bd495c40bdd5b54423262bc1ee1edbdd7
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-05-02 18:59:49 +0300

    add ui state class

---
+++ app/src/main/java/com/example/marvel_hub/data/model/UiState.kt
@@ -0,0 +1,9 @@
+package com.example.marvel_hub.data.model
+
+sealed class UiState<out T> {
+    data class Success<T>(val data: T) : UiState<T>()
+    data class Error(val message: String) : UiState<Nothing>()
+    object Loading : UiState<Nothing>()
+
+    fun toData(): T? = if (this is Success) data else null
+}
