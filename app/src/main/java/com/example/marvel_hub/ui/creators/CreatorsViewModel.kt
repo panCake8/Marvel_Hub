a18f7ee36bccdcd2f254efd4576a5721f61c895d
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-05-07 21:29:58 +0300

    update CreatorsViewModel

--- app/src/main/java/com/example/marvel_hub/ui/creators/CreatorsViewModel.kt
+++ app/src/main/java/com/example/marvel_hub/ui/creators/CreatorsViewModel.kt
@@ -5,18 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import com.example.marvel_hub.data.model.CreatorModel
 import com.example.marvel_hub.data.model.base.BaseResponse
 import com.example.marvel_hub.ui.base.BaseViewModel
+import com.example.marvel_hub.ui.util.UiState
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class CreatorsViewModel : BaseViewModel() {
 
-    private val _creator = MutableLiveData<BaseResponse<CreatorModel>>()
-    val creator: LiveData<BaseResponse<CreatorModel>>
+
+    private val _creator = MutableLiveData<UiState<BaseResponse<CreatorModel?>>>(UiState.Loading)
+    val creator: LiveData<UiState<BaseResponse<CreatorModel?>>>
         get() = _creator
 
     fun getCreator() {
         disposable.add(
-            repository.getAllCreators()
+            repository.getAllCharacters()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(::onGetCreatorsSuccess, ::onGetCreatorsFail)
@@ -24,11 +26,13 @@ class CreatorsViewModel : BaseViewModel() {
 
     }
 
-    private fun onGetCreatorsSuccess(creators: BaseResponse<CreatorModel>) {
-        _creator.postValue( creators)
+    private fun onGetCreatorsSuccess(creators: BaseResponse<CreatorModel?>) {
+        _creator.postValue( UiState.Success(creators))
 
     }
 
-    private fun onGetCreatorsFail(throwable: Throwable) {
+    private fun onGetCreatorsFail(error: Throwable) {
+        _creator.postValue(UiState.Error(error.message.toString()))
     }
+
 }
