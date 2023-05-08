e15318fe6d014dbbb72b0ee6e6fe312ad747e2aa
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-05-08 11:00:17 +0300

    update creator view model

--- app/src/main/java/com/example/marvel_hub/ui/creators/CreatorsViewModel.kt
+++ app/src/main/java/com/example/marvel_hub/ui/creators/CreatorsViewModel.kt
@@ -3,22 +3,22 @@ package com.example.marvel_hub.ui.creators
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.marvel_hub.data.model.CreatorModel
-import com.example.marvel_hub.data.model.base.BaseResponse
 import com.example.marvel_hub.ui.base.BaseViewModel
-import com.example.marvel_hub.ui.util.UiState
+import com.example.marvel_hub.data.util.DataState
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class CreatorsViewModel : BaseViewModel() {
 
 
-    private val _creator = MutableLiveData<UiState<BaseResponse<CreatorModel?>>>(UiState.Loading)
-    val creator: LiveData<UiState<BaseResponse<CreatorModel?>>>
+    private val _creator =
+        MutableLiveData<DataState<CreatorModel?>>(DataState.Loading)
+    val creator: LiveData<DataState<CreatorModel?>>
         get() = _creator
 
-    fun getCreator() {
+    fun getCreator( id :Int) {
         disposable.add(
-            repository.getAllCharacters()
+            repository.getCreatorById(id)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(::onGetCreatorsSuccess, ::onGetCreatorsFail)
@@ -26,13 +26,13 @@ class CreatorsViewModel : BaseViewModel() {
 
     }
 
-    private fun onGetCreatorsSuccess(creators: BaseResponse<CreatorModel?>) {
-        _creator.postValue( UiState.Success(creators))
+    private fun onGetCreatorsSuccess(creators: CreatorModel?) {
+        _creator.postValue(DataState.Success(creators))
 
     }
 
     private fun onGetCreatorsFail(error: Throwable) {
-        _creator.postValue(UiState.Error(error.message.toString()))
+        _creator.postValue(DataState.Error(error.message.toString()))
     }
 
 }
