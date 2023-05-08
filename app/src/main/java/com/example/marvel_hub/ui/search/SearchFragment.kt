f5c64b81b14079308bc69068bd44d3ec38710d3c
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-05-08 23:30:13 +0300

    add setOnQueryTextListener

--- app/src/main/java/com/example/marvel_hub/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/marvel_hub/ui/search/SearchFragment.kt
@@ -1,15 +1,47 @@
 package com.example.marvel_hub.ui.search
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.viewModels
 import com.example.marvel_hub.R
 import com.example.marvel_hub.databinding.FragmentSearchBinding
 import com.example.marvel_hub.ui.base.BaseFragment
 import com.example.marvel_hub.ui.search.viewModel.SearchViewModel
 
-class SearchFragment: BaseFragment<FragmentSearchBinding, SearchViewModel>() {
+class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
 
     override val viewModel: SearchViewModel by viewModels()
 
     override val layoutId: Int
         get() = R.layout.fragment_search
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchBar.setOnQueryTextListener(object : OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+
+        })
+
+    }
+
+
+    private fun filterList(query:String?){
+        if(query != null){
+
+
+        }
+    }
 }
