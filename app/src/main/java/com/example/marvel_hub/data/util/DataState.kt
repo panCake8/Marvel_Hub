f38b2ccef39d8a6c905f4e791fd940917d63aaa1
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-05-08 07:03:14 +0300

    fix issue

--- app/src/main/java/com/example/marvel_hub/data/util/DataState.kt
+++ app/src/main/java/com/example/marvel_hub/data/util/DataState.kt
@@ -1,9 +1,9 @@
 package com.example.marvel_hub.data.util
 
 sealed class DataState<out T> {
-    data class Success<T>(val data: List<T>) : DataState<T>()
+    data class Success<T>(val data: T) : DataState<T>()
     data class Error(val message: String) : DataState<Nothing>()
     object Loading : DataState<Nothing>()
 
-    fun toData(): List<T>? = if (this is Success) data else null
+    fun toData(): T? = if (this is Success) data else null
 }
