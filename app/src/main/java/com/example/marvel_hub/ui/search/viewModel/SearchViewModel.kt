68a62c8f99ef753b16c6e0457b9a01cc140ebc63
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-05-09 17:57:13 +0300

    handel search functions

--- app/src/main/java/com/example/marvel_hub/ui/search/viewModel/SearchViewModel.kt
+++ app/src/main/java/com/example/marvel_hub/ui/search/viewModel/SearchViewModel.kt
@@ -2,28 +2,66 @@ package com.example.marvel_hub.ui.search.viewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.marvel_hub.data.model.BaseResponse
 import com.example.marvel_hub.data.model.ComicModel
+import com.example.marvel_hub.data.model.CreatorModel
+import com.example.marvel_hub.data.model.EventModel
 import com.example.marvel_hub.data.util.DataState
 import com.example.marvel_hub.ui.base.BaseViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class SearchViewModel: BaseViewModel() {
-    private val _comics = MutableLiveData<DataState<ComicModel?>>(DataState.Loading)
-    val comics :LiveData<DataState<ComicModel?>>
-        get() = _comics
-    fun getAllComics(){
+class SearchViewModel : BaseViewModel() {
+
+    private val _comics = MutableLiveData<DataState<BaseResponse<ComicModel>>>(DataState.Loading)
+    val comics: LiveData<DataState<BaseResponse<ComicModel>>> = _comics
+
+    private val _creators = MutableLiveData<DataState<BaseResponse<CreatorModel>>>(DataState.Loading)
+    val creators: LiveData<DataState<BaseResponse<CreatorModel>>> = _creators
+
+    private val _event = MutableLiveData<DataState<BaseResponse<EventModel>>>(DataState.Loading)
+    val event: LiveData<DataState<BaseResponse<EventModel>>> = _event
+
+    fun searchInComics(text: String) {
         disposable.add(
-            repository.getAllComics()
+            repository.searchComics(text)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(::onGetComicsSuccess,::onGetComicsError)
+                .subscribe(::onGetComicsSuccess, ::onGetComicsError)
         )
     }
-    private fun onGetComicsSuccess(comics:ComicModel){
-        _comics.postValue(DataState.Success(comics.))
-    }
-    private fun onGetComicsError(){
 
+    fun searchInEvent(text: String) {
+        disposable.add(
+            repository.searchEvents(text)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(::onGetEventsSuccess, ::onGetEventsError)
+        )
     }
+
+    fun searchInCreators(text: String) {
+        disposable.add(
+            repository.searchCreators(text)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(::onGetCreatorsSuccess, ::onGetCreatorsError)
+        )
+    }
+
+    private fun onGetComicsSuccess(comics: BaseResponse<ComicModel>) = _comics.postValue(DataState.Success(comics))
+
+    private fun onGetComicsError(throwable: Throwable) = DataState.Error(throwable.message.toString())
+
+
+    private fun onGetCreatorsSuccess(creators:BaseResponse<CreatorModel>) = _creators.postValue(DataState.Success(creators))
+
+    private fun onGetCreatorsError(throwable: Throwable) = DataState.Error(throwable.message.toString())
+
+
+    private fun onGetEventsSuccess(events: BaseResponse<EventModel>) = _event.postValue(DataState.Success(events))
+
+    private fun onGetEventsError(throwable: Throwable) = DataState.Error(throwable.message.toString())
+
+
 }
