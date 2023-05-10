5e0aedab58c50d8c77c643062249246fb05f5856
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-05-10 23:30:52 +0300

    fix issue

--- app/src/main/java/com/example/marvel_hub/ui/stories/StoriesAdapter.kt
+++ app/src/main/java/com/example/marvel_hub/ui/stories/StoriesAdapter.kt
@@ -4,7 +4,8 @@ import com.example.marvel_hub.R
 import com.example.marvel_hub.data.model.StoriesModel
 import com.example.marvel_hub.ui.base.BaseAdapter
 
-class StoriesAdapter(stories: List<StoriesModel>,listener: StoriesInteractionListener):BaseAdapter<StoriesModel>(stories,listener) {
+class StoriesAdapter(stories: List<StoriesModel>,listener: StoriesInteractionListener)
+    :BaseAdapter<StoriesModel>(stories,listener) {
     override val getLayoutId: Int
         get() = R.layout.item_story_card
 }
