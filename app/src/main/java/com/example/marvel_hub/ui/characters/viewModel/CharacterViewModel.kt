827878036b4ea719bd89fdd3cff9bcb6b205a86f
Bassam <bassamouf@std.mans.edu.eg>
2023-05-11 00:21:41 +0300

    handle clean code character

--- app/src/main/java/com/example/marvel_hub/ui/characters/viewModel/CharacterViewModel.kt
+++ app/src/main/java/com/example/marvel_hub/ui/characters/viewModel/CharacterViewModel.kt
@@ -18,8 +18,8 @@ class CharacterViewModel : BaseViewModel(), CharacterInteractionListener {
         MutableLiveData<DataState<BaseResponse<CharactersModel>>>(DataState.Loading)
     val characterMarvel: LiveData<DataState<BaseResponse<CharactersModel>>> = _characterMarvel
 
-    private val _eventCharacter = MutableLiveData<Event<Int?>>()
-    val eventCharacter: LiveData<Event<Int?>> get() = _eventCharacter
+    private val _selectedCharacterItem = MutableLiveData<Event<Int?>>()
+    val selectedCharacterItem: LiveData<Event<Int?>> get() = _selectedCharacterItem
 
     init {
         getAllCharactersMarvel()
@@ -42,7 +42,7 @@ class CharacterViewModel : BaseViewModel(), CharacterInteractionListener {
     }
 
     override fun onClickCharacter(character: CharactersModel) {
-        _eventCharacter.postValue(Event(character.id))
+        _selectedCharacterItem.postValue(Event(character.id))
     }
 
 }
