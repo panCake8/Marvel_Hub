096db59e3c3366f5a383f87efee13382fb284225
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-05-10 23:48:00 +0300

    add searchItems

---
+++ app/src/main/java/com/example/marvel_hub/ui/search/SearchItems.kt
@@ -0,0 +1,13 @@
+package com.example.marvel_hub.ui.search
+
+import com.example.marvel_hub.data.model.ComicModel
+import com.example.marvel_hub.data.model.EventModel
+import com.example.marvel_hub.data.model.SeriesModel
+
+
+sealed class SearchItems(rank: Int) {
+    data class Comics(val data: List<ComicModel>) : SearchItems(0)
+    data class Events(val data: List<EventModel>) : SearchItems(1)
+    data class Series(val data: List<SeriesModel>) : SearchItems(2)
+}
+
