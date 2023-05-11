986d960d540657bfe97b44587e91c0d43fe88ff0
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-05-11 17:28:11 +0300

    update series adapters

--- app/src/main/java/com/example/marvel_hub/ui/details/series/adapters/ChildSeriesStoriesAdapter.kt
+++ app/src/main/java/com/example/marvel_hub/ui/details/series/adapters/ChildSeriesStoriesAdapter.kt
@@ -1,11 +1,11 @@
 package com.example.marvel_hub.ui.details.series.adapters
 
 import com.example.marvel_hub.R
-import com.example.marvel_hub.data.model.SeriesModel
 import com.example.marvel_hub.data.model.StoriesModel
 import com.example.marvel_hub.ui.base.BaseAdapter
+import com.example.marvel_hub.ui.details.listeners.StoryListener
 
-class ChildSeriesStoriesAdapter(items: List<StoriesModel>, listener: BaseAdapterListener) :
+class ChildSeriesStoriesAdapter(items: List<StoriesModel>, listener: StoryListener) :
     BaseAdapter<StoriesModel>(items, listener) {
-    override val getLayoutId = R.layout.item_comics
+    override val getLayoutId = R.layout.item_details_stories
 }
