663e9765a7424a4ff396c9498451cb6b5fb29866
kareem <skrym826@gmail.com>
2023-05-12 20:12:40 +0300

    add DataBinding and ViewModel

--- app/src/main/java/com/example/marvel_hub/ui/events/viewModel/EventsViewModel.kt
+++ app/src/main/java/com/example/marvel_hub/ui/events/viewModel/EventsViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.marvel_hub.ui.events.viewModel
 
-import android.annotation.SuppressLint
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,41 +7,49 @@ import com.example.marvel_hub.data.model.BaseResponse
 import com.example.marvel_hub.data.model.EventModel
 import com.example.marvel_hub.data.util.DataState
 import com.example.marvel_hub.ui.base.BaseViewModel
+import com.example.marvel_hub.ui.events.event_recycler.EventsSeeAllListener
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class EventsViewModel : BaseViewModel() {
+class EventsViewModel : BaseViewModel(), EventsSeeAllListener {
 
-    private val _events = MutableLiveData<DataState<BaseResponse<EventModel>>>()
-
-
-    val event : LiveData<DataState<BaseResponse<EventModel>>>
+    private val _events = MutableLiveData<DataState<List<EventModel>>>()
+    val event: LiveData<DataState<List<EventModel>>>
         get() = _events
-
 
 
     init {
         fetchAllEvents()
     }
 
-    @SuppressLint("CheckResult")
     fun fetchAllEvents() {
         repository.getAllEvents()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(::success, ::failure).addTo(disposable)
+            .subscribe(::onEventSuccess, ::onError)
+            .addTo(disposable)
     }
 
-    private fun success(response: BaseResponse<EventModel>) {
-        Log.i("TAG", "response Successful : ${response.data?.results?.size}")
-        _events.postValue(DataState.Success(response))
+    private fun onEventSuccess(events: BaseResponse<EventModel>) {
+
+        Log.i("TAG", "response Successful : ${events.data?.results?.size}")
+        _events.postValue(events.data?.results?.let { DataState.Success(it) })
 
     }
 
-    private fun failure(T: Throwable) {
+    private fun onError(T: Throwable) {
         Log.i("TAG", "failed : ${T.message}")
+        _events.postValue(T.message.toString().let { DataState.Error(it) })
+    }
 
+    override fun onClick(event: EventModel) {
+//        val fragment = EventsDetails()
+//
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.fragment_container, fragment)
+//            .addToBackStack(null)
+//            .commit()
     }
 
 }
