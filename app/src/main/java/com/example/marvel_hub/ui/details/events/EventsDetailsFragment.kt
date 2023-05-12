02e3f0dbac955e4ef43fb41c37f0670d24190beb
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-05-12 18:52:18 +0300

    setup parent adapter in main recycler view

--- app/src/main/java/com/example/marvel_hub/ui/details/events/EventsDetailsFragment.kt
+++ app/src/main/java/com/example/marvel_hub/ui/details/events/EventsDetailsFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.viewModels
 import com.example.marvel_hub.R
 import com.example.marvel_hub.databinding.FragmentEventsDetailsBinding
 import com.example.marvel_hub.ui.base.BaseFragment
+import com.example.marvel_hub.ui.details.events.adapter.ParentEventsAdapter
 
 class EventsDetailsFragment: BaseFragment<FragmentEventsDetailsBinding,EventsDetailsViewModel>() {
     override val viewModel: EventsDetailsViewModel by viewModels()
@@ -14,5 +15,15 @@ class EventsDetailsFragment: BaseFragment<FragmentEventsDetailsBinding,EventsDet
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpAdapter()
+        initArguments()
+
+    }
+    private fun setUpAdapter(){
+        binding.mainRecycler.adapter = ParentEventsAdapter(viewModel, viewLifecycleOwner)
+    }
+
+    private fun initArguments(){
+        //viewModel.getEventById()
     }
 }
