f7935f520ee4eb2aacc4c1645519737b8cef2492
youssef <yr925344git@gmail.com>
2023-05-12 23:15:53 +0300

    add BaseFragment

--- app/src/main/java/com/example/marvel_hub/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/marvel_hub/ui/game/GameFragment.kt
@@ -1,10 +1,21 @@
 package com.example.marvel_hub.ui.game
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.example.marvel_hub.R
+import com.example.marvel_hub.databinding.FragmentGameBinding
+import com.example.marvel_hub.ui.base.BaseFragment
+import com.example.marvel_hub.ui.game.viewmodel.GameViewModel
 
 
-class GameFragment : Fragment() {
+class GameFragment : BaseFragment<FragmentGameBinding, GameViewModel>() {
+    override val viewModel: GameViewModel by viewModels()
 
+    override val layoutId: Int
+        get() = R.layout.fragment_game
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+    }
 }
