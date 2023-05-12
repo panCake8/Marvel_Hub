56963b66a37cae2eb4627dfe67abc73dbb6fdf2f
kareem <skrym826@gmail.com>
2023-05-12 22:36:30 +0300

    fix issues

--- app/src/main/java/com/example/marvel_hub/ui/events/viewModel/EventsViewModel.kt
+++ app/src/main/java/com/example/marvel_hub/ui/events/viewModel/EventsViewModel.kt
@@ -4,21 +4,30 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.marvel_hub.data.model.BaseResponse
+import com.example.marvel_hub.data.model.ComicModel
 import com.example.marvel_hub.data.model.EventModel
-import com.example.marvel_hub.data.util.DataState
 import com.example.marvel_hub.ui.base.BaseViewModel
 import com.example.marvel_hub.ui.events.event_recycler.EventsSeeAllListener
+import com.example.marvel_hub.util.Event
+import com.example.marvel_hub.util.State
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class EventsViewModel : BaseViewModel(), EventsSeeAllListener {
 
-    private val _events = MutableLiveData<DataState<List<EventModel>>>()
-    val event: LiveData<DataState<List<EventModel>>>
+    private val _events = MutableLiveData<State<List<EventModel>>>()
+    val event: LiveData<State<List<EventModel>>>
         get() = _events
 
 
+    private val _selectedEventItem = MutableLiveData<Event<EventModel>>()
+    val selectedEventItem: LiveData<Event<EventModel>>
+        get() = _selectedEventItem
+
+    init {
+        fetchAllEvents()
+    }
 
 
     fun fetchAllEvents() {
@@ -32,16 +41,19 @@ class EventsViewModel : BaseViewModel(), EventsSeeAllListener {
     private fun onEventSuccess(events: BaseResponse<EventModel>) {
 
         Log.i("TAG", "response Successful : ${events.data?.results?.size}")
-        _events.postValue(events.data?.results?.let { DataState.Success(it) })
+        _events.postValue(events.data?.results?.let { State.Success(it) })
 
     }
 
     private fun onError(T: Throwable) {
         Log.i("TAG", "failed : ${T.message}")
-        _events.postValue(T.message.toString().let { DataState.Error(it) })
+        _events.postValue(T.message.toString().let { State.Error(it) })
     }
 
     override fun onClick(event: EventModel) {
+
+        _selectedEventItem.postValue(Event(event))
+
 //        val fragment = EventsDetails()
 //
 //        supportFragmentManager.beginTransaction()
