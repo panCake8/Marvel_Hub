53a40fb09eb469979456dca321d91350e06603f0
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-05-13 19:30:56 +0300

    add series events

--- app/src/main/java/com/example/marvel_hub/ui/details/series/SeriesDetailsFragment.kt
+++ app/src/main/java/com/example/marvel_hub/ui/details/series/SeriesDetailsFragment.kt
@@ -3,6 +3,8 @@ package com.example.marvel_hub.ui.details.series
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.marvel_hub.R
 import com.example.marvel_hub.databinding.FragmentSeriesDetailsBinding
 import com.example.marvel_hub.ui.base.BaseFragment
@@ -12,6 +14,7 @@ import com.example.marvel_hub.util.EventObserver
 class SeriesDetailsFragment : BaseFragment<FragmentSeriesDetailsBinding, SeriesDetailsViewModel>() {
     override val viewModel: SeriesDetailsViewModel by viewModels()
     override val layoutId = R.layout.fragment_series_details
+    val argument: SeriesDetailsFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,17 +28,53 @@ class SeriesDetailsFragment : BaseFragment<FragmentSeriesDetailsBinding, SeriesD
 
     private fun setupAdapter() {
         binding.mainRecycler.adapter = ParentSeriesAdapter(viewModel, viewLifecycleOwner)
-        viewModel.getSeriesById(26024)
-        viewModel.getCharactersBySeriesId(26024)
-        viewModel.getComicsBySeriesId(26024)
-        viewModel.getStoriesBySeriesId(26024)
-        viewModel.getEventsBySeriesId(26024)
+        val id = argument.id
+        viewModel.getSeriesById(id)
+        viewModel.getCharactersBySeriesId(id)
+        viewModel.getComicsBySeriesId(id)
+        viewModel.getStoriesBySeriesId(id)
+        viewModel.getEventsBySeriesId(id)
 
     }
-    private fun observeEvents() {
-        viewModel.characterEvent.observe(viewLifecycleOwner, EventObserver{
-            if (it != null){
 
+    private fun observeEvents() {
+        viewModel.characterEvent.observe(viewLifecycleOwner, EventObserver {
+            if (it != null) {
+                val nav =
+                    SeriesDetailsFragmentDirections.actionSeriesDetailsFragmentToCharacterDetailsFragment(
+                        it.id!!
+                    )
+                findNavController().navigate(nav)
+            }
+        })
+
+        viewModel.eventEvent.observe(viewLifecycleOwner, EventObserver {
+            if (it != null) {
+                val nav =
+                    SeriesDetailsFragmentDirections.actionSeriesDetailsFragmentToEventsDetailsFragment(
+                        it.id!!
+                    )
+                findNavController().navigate(nav)
+            }
+        })
+
+        viewModel.comicEvent.observe(viewLifecycleOwner, EventObserver {
+            if (it != null) {
+                val nav =
+                    SeriesDetailsFragmentDirections.actionSeriesDetailsFragmentToComicsDetailsFragment(
+                        it.id!!
+                    )
+                findNavController().navigate(nav)
+            }
+        })
+
+        viewModel.storiesEvent.observe(viewLifecycleOwner, EventObserver {
+            if (it != null) {
+                val nav =
+                    SeriesDetailsFragmentDirections.actionSeriesDetailsFragmentToStoriesDetailsFragment(
+                        it.id!!
+                    )
+                findNavController().navigate(nav)
             }
         })
     }
