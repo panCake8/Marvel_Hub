281722c0471ef15affb6b0214dcd404b3f8d4114
youssef <yr925344git@gmail.com>
2023-05-13 15:18:13 +0300

    update styles

--- app/src/main/java/com/example/marvel_hub/util/Extensions.kt
+++ app/src/main/java/com/example/marvel_hub/util/Extensions.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.MutableLiveData
 import com.example.marvel_hub.data.enums.AnswerState
 import com.example.marvel_hub.data.model.AnswerModel
 import com.example.marvel_hub.data.model.CharactersModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun CharactersModel.toAnswer(isCorrect: Boolean, answerState: AnswerState = AnswerState.UNSELECTED, isDeleted : Boolean = false) =
     AnswerModel(this.name!!, isCorrect, answerState, isDeleted)
@@ -13,7 +16,9 @@ fun CharactersModel.toAnswer(isCorrect: Boolean, answerState: AnswerState = Answ
 fun <T> MutableLiveData<Event<T>>.postEvent(content: T) {
     postValue(Event(content))
 }
-
+fun <T : Any> Observable<T>.observeOnMainThread(): Observable<T> {
+    return subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+}
 inline fun <T> LiveData<Event<T>>.observeEvent(owner: LifecycleOwner, crossinline onEventUnhandledContent: (T) -> Unit) {
     observe(owner) { it?.getContentIfNotHandled()?.let(onEventUnhandledContent) }
 }
