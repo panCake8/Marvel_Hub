6afd5927426a3af26aa73afcf8c7a73e86d4258c
youssef <yr925344git@gmail.com>
2023-05-13 06:26:32 +0300

    add post event extension

--- app/src/main/java/com/example/marvel_hub/util/Extensions.kt
+++ app/src/main/java/com/example/marvel_hub/util/Extensions.kt
@@ -1,8 +1,13 @@
 package com.example.marvel_hub.util
 
+import androidx.lifecycle.MutableLiveData
 import com.example.marvel_hub.data.enums.AnswerState
 import com.example.marvel_hub.data.model.AnswerModel
 import com.example.marvel_hub.data.model.CharactersModel
 
 fun CharactersModel.toAnswer(isCorrect: Boolean, answerState: AnswerState = AnswerState.UNSELECTED, isDeleted : Boolean = false) =
     AnswerModel(this.name!!, isCorrect, answerState, isDeleted)
+
+fun <T> MutableLiveData<Event<T>>.postEvent(content: T) {
+    postValue(Event(content))
+}
