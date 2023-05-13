8047ae3b92c1b7918a80bc8f892c22405d8f4ccf
youssef <yr925344git@gmail.com>
2023-05-13 06:22:06 +0300

    add Utils file

---
+++ app/src/main/java/com/example/marvel_hub/util/Utils.kt
@@ -0,0 +1,10 @@
+package com.example.marvel_hub.util
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
+import java.util.concurrent.TimeUnit
+
+fun doAfterDelay(delay: Long, timeUnit: TimeUnit = TimeUnit.SECONDS, lambda: () -> Unit) {
+    Single.timer(delay, timeUnit).observeOn(AndroidSchedulers.mainThread())
+        .map { lambda() }.subscribe()
+}
